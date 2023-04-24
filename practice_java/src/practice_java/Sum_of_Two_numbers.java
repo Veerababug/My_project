c8367a40432cc83c30de43c057635197d3074913
Veerababug <veerababug126@gmail.com>
2023-04-24 23:57:14 +0530

    commiting code

---
+++ practice_java/src/practice_java/Sum_of_Two_numbers.java
@@ -0,0 +1,15 @@
+package practice_java;
+
+public class Sum_of_Two_numbers {
+	int x=200  ,y=355; 
+	
+	public static void main(String []args) {
+	int n1=299, n2=333,sum;
+	sum =n1+n2;
+		
+		System.out.println("The sum of two numbers is "+sum); 
+		
+		
+	}
+
+}
